1b935a5fc5c502ad6aaa759b352843c04abbf863
squeemishly <squeemishly@gmail.com>
2017-04-26 11:41:44 -0600

    still getting the same number every time I refresh

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x = rand(100)
+NUMBER = rand(100)
+
+def messager(answer)
+  if answer.nil?
+    message = "Guess the secret NUMBER in the form below. The secret NUMBER is #{NUMBER}"
+  elsif answer.to_i > NUMBER
+    message = "You guessed too high! Try again! The secret NUMBER is #{NUMBER}"
+  elsif answer.to_i < NUMBER
+    message = "You guessed too low! Try again! The secret NUMBER is #{NUMBER}"
+  elsif answer.to_i == NUMBER
+    message = "You guessed correctly! Congratulations!"
+  end
+end
 
 get '/' do
-  "The secrect number is #{x}"
+  answer = params['guess']
+  erb :index, :locals => {:message => messager(answer)}
 end
